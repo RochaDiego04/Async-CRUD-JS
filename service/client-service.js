4397fee6e5571002042aa62b958f49504cfdf996
Diego Rocha <drochaesc@gmail.com>
2023-07-30 15:08:18 -0600

    Separando archivos servicios y controladores
    servicios (conexion a la db) y controladores (recibir datos y generar HTML)

--- service/client-service.js
+++ service/client-service.js
@@ -1,32 +1,3 @@
-const crearNuevaLinea = (nombre, email) => {
-  const linea = document.createElement("TR");
-  const contenido = `
-    <td class="td" data-td>${nombre}</td>
-    <td>${email}</td>
-    <td>
-      <ul class="table__button-control">
-        <li>
-          <a
-            href="../screens/editar_cliente.html"
-            class="simple-button simple-button--edit"
-            >Editar</a
-          >
-        </li>
-        <li>
-          <button
-            class="simple-button simple-button--delete"
-            type="button"
-          >
-            Eliminar
-          </button>
-        </li>
-      </ul>
-    </td>`;
-  linea.innerHTML = contenido;
-  return linea;
-};
-
-const table = document.querySelector("[data-table]");
 
 // Abrir HTTP (método, url)
 
@@ -40,10 +11,6 @@ Delete - DELETE
 const listaClientes = () => // Fetch utiliza GET por defecto
     fetch("http://localhost:3000/perfil").then( respuesta => respuesta.json());
 
-listaClientes().then(data => {
-    data.forEach((perfil) => {
-        const nuevaLinea = crearNuevaLinea(perfil.nombre, perfil.email);
-        table.appendChild(nuevaLinea);
-      });
-})
-.catch((error) => alert(`Ocurrió un error: ${error}`));
+export const clientServices = {
+  listaClientes
+};
